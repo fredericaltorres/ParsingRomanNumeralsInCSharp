dcbb39cccb663ede0122e6ca23a9bd2ec6f6bf07
Frederic Torres <fredericaltorres@gmail.com>
2019-05-12 00:51:18 -0400

    Creation, my take

---
+++ RomanNumeralsParser.Tests/RomanNumeralsParser.UnitTests.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using NUnit.Framework;
+using System;
+
+namespace RomanNumerals
+{
+    [TestFixture]
+    public class RomanNumeralsParser_UnitTests
+    {
+	    [SetUp]
+	    public void SetUp()
+	    {
+
+	    }
+
+        [TestCase("MMM", 3000)]
+        [TestCase("MM", 2000)]
+        [TestCase("M", 1000)]
+        [TestCase("CM", 900)]
+        [TestCase("DCCC", 800)]
+        [TestCase("DCC", 700)]
+        [TestCase("DC", 600)]
+        [TestCase("D", 500)]
+        [TestCase("CD", 400)]
+        [TestCase("CCC", 300)]
+        [TestCase("CC", 200)]
+        [TestCase("C", 100)]
+        [TestCase("XC", 90)]
+        [TestCase("LXXX", 80)]
+        [TestCase("LXX", 70)]
+        [TestCase("LX", 60)]
+        [TestCase("L", 50)]
+        [TestCase("XL", 40)]
+        [TestCase("XXX", 30)]
+        [TestCase("XX", 20)]
+        [TestCase("X", 10)]
+        [TestCase("IX", 9)]
+        [TestCase("VIII", 8)]
+        [TestCase("VII", 7)]
+        [TestCase("VI", 6)]
+        [TestCase("V", 5)]
+        [TestCase("IV", 4)]
+        [TestCase("III", 3)]
+        [TestCase("II", 2)]
+        [TestCase("I", 1)]
+        public void Parse_Thousand_Hundred_Ten(string numeralString, int expected)
+        {
+	        var converter = new RomanNumerals.Parser();
+
+	        var result = converter.ToInt(numeralString);
+
+	        Assert.AreEqual(result, expected);
+        }
+
+        [TestCase("MMMI", 3001)]
+        [TestCase("MMMXII", 3012)]
+        [TestCase("MMMCXXIII", 3123)]
+        
+        [TestCase("MM", 2000)]
+        [TestCase("M", 1000)]
+        [TestCase("CM", 900)]
+        [TestCase("DCCC", 800)]
+        [TestCase("DCC", 700)]
+        [TestCase("DC", 600)]
+        [TestCase("D", 500)]
+        [TestCase("CD", 400)]
+        [TestCase("CCC", 300)]
+        [TestCase("CC", 200)]
+        [TestCase("C", 100)]
+        [TestCase("XC", 90)]
+        [TestCase("LXXX", 80)]
+        [TestCase("LXX", 70)]
+        [TestCase("LX", 60)]
+        [TestCase("L", 50)]
+        [TestCase("XL", 40)]
+        [TestCase("XXX", 30)]
+        [TestCase("XX", 20)]
+        [TestCase("X", 10)]
+        [TestCase("IX", 9)]
+        [TestCase("VIII", 8)]
+        [TestCase("VII", 7)]
+        [TestCase("VI", 6)]
+        [TestCase("V", 5)]
+        [TestCase("IV", 4)]
+        [TestCase("III", 3)]
+        [TestCase("II", 2)]
+        [TestCase("I", 1)]
+        public void Parse_Numbers(string numeralString, int expected)
+        {
+            var converter = new RomanNumerals.Parser();
+
+            var result = converter.ToInt(numeralString);
+
+            Assert.AreEqual(result, expected);
+        }
+
+    }
+}
---
+++ RomanNumeralsParser/RomanNumeralsParser.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+
+namespace RomanNumerals
+{
+    public class Parser
+    {
+        public int ToInt(string roman)
+        {
+            if (string.IsNullOrEmpty(roman)) // Expression evaluated with success
+                return 0;
+
+            roman = roman.ToUpperInvariant();
+
+            if (roman.StartsWith("MMMM")) // 4000
+                return 4000 + this.ToInt(roman.Substring(3));
+            else if (roman.StartsWith("MMM")) // 3000
+                return 3000 + this.ToInt(roman.Substring(3));
+            else if (roman.StartsWith("MM"))  // 2000
+                return 2000 + this.ToInt(roman.Substring(2));
+            else if (roman.StartsWith("M"))  // 1000
+                return 1000 + this.ToInt(roman.Substring(1));
+
+            else if (roman.StartsWith("CM")) // 900
+                return 900 + this.ToInt(roman.Substring(2));
+            else if (roman.StartsWith("DCCC")) // 800
+                return 800 + this.ToInt(roman.Substring(4));
+            else if (roman.StartsWith("DCC")) // 700
+                return 700 + this.ToInt(roman.Substring(3));
+            else if (roman.StartsWith("DC")) // 600
+                return 600 + this.ToInt(roman.Substring(2));
+            else if (roman.StartsWith("D")) // 500
+                return 500 + this.ToInt(roman.Substring(1));
+            else if (roman.StartsWith("CD")) // 400
+                return 400 + this.ToInt(roman.Substring(2));
+            else if (roman.StartsWith("CCC")) // 300
+                return 300 + this.ToInt(roman.Substring(3));
+            else if (roman.StartsWith("CC")) // 200
+                return 200 + this.ToInt(roman.Substring(2));
+            else if (roman.StartsWith("C")) // 100
+                return 100 + this.ToInt(roman.Substring(1));
+
+            else if (roman.StartsWith("XC")) // 90
+                return 90 + this.ToInt(roman.Substring(2));
+            else if (roman.StartsWith("LXXX")) // 80
+                return 80 + this.ToInt(roman.Substring(4));
+            else if (roman.StartsWith("LXX")) // 70
+                return 70 + this.ToInt(roman.Substring(3));
+            else if (roman.StartsWith("LX")) // 60
+                return 60 + this.ToInt(roman.Substring(2));
+            else if (roman.StartsWith("L")) // 50
+                return 50 + this.ToInt(roman.Substring(1));
+            else if (roman.StartsWith("XL")) // 40
+                return 40 + this.ToInt(roman.Substring(2));
+            else if (roman.StartsWith("XXX")) // 30
+                return 30 + this.ToInt(roman.Substring(3));
+            else if (roman.StartsWith("XX")) // 20
+                return 20 + this.ToInt(roman.Substring(2));
+            else if (roman.StartsWith("X")) // 10
+                return 10 + this.ToInt(roman.Substring(1));
+
+            else if (roman.StartsWith("IX")) // 9
+                return 9 + this.ToInt(roman.Substring(2));
+            else if (roman.StartsWith("VIII")) // 8
+                return 8 + this.ToInt(roman.Substring(4));
+            else if (roman.StartsWith("VII")) // 7
+                return 7 + this.ToInt(roman.Substring(3));
+            else if (roman.StartsWith("VI")) // 6
+                return 6 + this.ToInt(roman.Substring(2));
+            else if (roman.StartsWith("V")) // 5
+                return 5 + this.ToInt(roman.Substring(1));
+            else if (roman.StartsWith("IV")) // 4
+                return 4 + this.ToInt(roman.Substring(2));
+            else if (roman.StartsWith("III")) // 3
+                return 3 + this.ToInt(roman.Substring(3));
+            else if (roman.StartsWith("II")) // 2
+                return 2 + this.ToInt(roman.Substring(2));
+            else if (roman.StartsWith("I")) // 1
+                return 1 + this.ToInt(roman.Substring(1));
+
+            return this.ThrowInvalidNumeralException(roman);
+        }
+
+	    public int ThrowInvalidNumeralException(string roman)
+	    {
+		    throw new Exception("Invalid Roman Numeral String: " + roman);
+	    }
+    }
+}
